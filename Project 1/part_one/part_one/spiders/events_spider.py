8ae0a70968729e3d8f1c3dba951c6e230013cd5c
peyman-alv <peyman.alv@gmail.com>
2020-10-19 20:07:43 +0330

    Added crawling next pages part

--- Project 1/part_one/part_one/spiders/events_spider.py
+++ Project 1/part_one/part_one/spiders/events_spider.py
@@ -5,12 +5,32 @@ from ..items import Event
 
 class EventsSpider(scrapy.Spider):
     name = "events"
+    allowed_domains = ['evand.com']
 
-    start_urls = ["https://evand.com/categories/%D8%AA%DA%A9%D9%86%D9%88%D9%84%D9%88%DA%98%DB%8C"]
+    def start_requests(self):
+        urls = ["https://evand.com/categories/%D8%AA%DA%A9%D9%86%D9%88%D9%84%D9%88%DA%98%DB%8C/"]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response, **kwargs):
-        event_page_links = response.css('section > a')
-        yield from response.follow_all(event_page_links, self.parse_event)
+        event_xpath = '//*[@id="root"]/div/div[2]/div[2]/div/div[2]/div/div/section[@*]/div/div/a'
+        # there are two possible xpath for next page ( first page and other pages )
+        next_url_xpath1 = '//*[@id="root"]/div/div[2]/div[2]/div/div[2]/div/nav/ul/li/a'
+        text_next_page = '//*[@id="root"]/div/div[2]/div[2]/div/div[2]/div/nav/ul/li/a/span'
+        next_url_xpath2 = '//*[@id="root"]/div/div[2]/div[2]/div/div[2]/div/nav/ul/li[2]/a'
+
+        # get all events link in a page at first
+        events = response.xpath(event_xpath)
+        yield from response.follow_all(events, self.parse_event)
+
+        # if next page exist it should be crawled as new link
+        next_page_url1 = response.xpath(next_url_xpath1)
+        next_page_url2 = response.xpath(next_url_xpath2)
+
+        if next_page_url1 is not None and 'صفحه بعد' in response.xpath(text_next_page).get():
+            yield response.follow(url=next_page_url1[0], callback=self.parse)
+        elif next_page_url2 is not None:
+            yield response.follow(url=next_page_url2[0], callback=self.parse)
 
     # a function with extracting html header text functionality
     @staticmethod
